4c566ddba8170a1cc73ce6f6fdc7594aba6853b7
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-08 17:29:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+	get '/newteam' do 
+		erb :newteam
+	end
+	# access URL
+
+	post '/newteam' do 
+		@team_name = params[:name]
+		@coach = params[:coach]
+		@pg = params[:pg]
+		@sg = params[:sg]
+		@sf = params[:sf]
+		@pf = params[:pf]
+		@c = params[:c]
+		erb:team
+	end
+	# when form is submitted 
+
 
 end
